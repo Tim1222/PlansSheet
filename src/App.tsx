0dfda5eb79fcbc14c3a1c8e4183f245f5e0ea6fb
Tim1222 <timofeu47@gmail.com>
2024-04-14 23:34:30 +0300

    1

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {TasksType, Todolist} from "./Todolist";
+
+function App() {
+
+    let tasks1: Array<TasksType> = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+    let tasks2: Array<TasksType> = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'Movie2', isDone: false},
+        {id: 3, title: 'Fortuna', isDone: true}
+    ]
+
+    return (
+        <div className='App'>
+            <Todolist title='What to learn' tasks={tasks1}/>
+            <Todolist title='Movies' tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
